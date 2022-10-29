9ce8099cfa6fe4299b5ff0cc45edd5f9dd297241
Amila Giragama <aminogira@gmail.com>
2022-10-29 07:27:41 +0530

    squares done

--- src/main/java/HRTwo.java
+++ src/main/java/HRTwo.java
@@ -24,4 +24,67 @@ public class HRTwo {
         System.out.println(bi);
     }
 
+    public static String appendAndDelete(String s, String t, int k) {
+        char[] ss,tt;
+        ss=s.toCharArray();
+        tt=t.toCharArray();
+        boolean isCountStartDelete=false;
+        boolean isCountStartAppend=false;
+
+        int nDelete=0;
+        int nAppend=0;
+        //int fullTotCount=0;
+        int nMax=ss.length;
+        int fullTotCount= nMax+tt.length;
+        if (tt.length>nMax) nMax=tt.length;
+        if(fullTotCount<=k){
+            return "Yes";
+        }else {
+            for (int i = 0; i < nMax; i++) {
+                if (tt.length > i && ss.length > i) {
+                    if (ss[i] != tt[i]) {
+                        isCountStartDelete = true;
+                        isCountStartAppend = true;
+                    }
+                } else {
+                    isCountStartDelete = true;
+                    isCountStartAppend = true;
+                }
+                if (isCountStartDelete && i < ss.length) {
+                    nDelete++;
+                }
+                if (isCountStartAppend && i < tt.length) {
+                    nAppend++;
+                }
+            }
+            System.out.println(nDelete + " :: " + nAppend);
+            if (nDelete + nAppend == k) {
+                return "Yes";
+            }else if (nDelete + nAppend < k) {
+                int dif= k - nDelete - nAppend;
+                if(dif%2==0) return "Yes";
+                else return "No";
+            } else {
+                return "No";
+            }
+        }
+    }
+
+
+    public static int squares(int a, int b) {
+        int ans=0;
+        double sqr=Math.sqrt(a);
+        int start= (int) Math.ceil(sqr);
+        boolean isInRange=true;
+        while(isInRange){
+            int power = start*start;
+            if(power>=a && power<=b){
+                ans++;
+            }else{
+                isInRange=false;
+            }
+            start++;
+        }
+        return ans;
+    }
 }
--- src/test/java/HRTwoTest.java
+++ src/test/java/HRTwoTest.java
@@ -28,5 +28,46 @@ class HRTwoTest {
         HRTwo.extraLongFactorials(n);
     }
 
+    @Test
+    public void testappendAndDelete01() {
+
+        String ans= HRTwo.appendAndDelete("hackerhappy", "hackerrank", 9);
+        assertEquals("Yes",ans);
+    }
+    @Test
+    public void testappendAndDelete02() {
+
+        String ans= HRTwo.appendAndDelete("aba", "aba", 7);
+        assertEquals("Yes",ans);
+    }
+    @Test
+    public void testappendAndDelete03() {
+
+        String ans= HRTwo.appendAndDelete("ashley", "ash", 2);
+        assertEquals("No",ans);
+    }
+    @Test
+    public void testappendAndDelete04() {
+        String ans= HRTwo.appendAndDelete("abcd", "abcdert", 10);
+        assertEquals("No",ans);
+    }
+    @Test
+    public void testappendAndDelete05() {
+        String ans= HRTwo.appendAndDelete("y", "yu", 2);
+        assertEquals("No",ans);
+    }
+
+    @Test
+    public void testsquares01() {
+        int ans= HRTwo.squares(3, 9);
+        assertEquals(2,ans);
+    }
+    @Test
+    public void testsquares02() {
+        int ans= HRTwo.squares(17, 24);
+        assertEquals(0,ans);
+    }
+
+
 
 }
